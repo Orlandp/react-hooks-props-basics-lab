35e4a23e01db6ab6cbefe2680ca93d6b6990a313
pknowledge <jamesblssings@gmail.com>
2024-11-04 00:12:11 +0300

    changes on th src hook props

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,29 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+const user = {
+  name: 'Liza',
+  city: 'New York',
+  bio: 'I made this!',
+  color: 'firebrick',
+  links: {
+    github: 'https://github.com/liza',
+    linkedin: 'https://www.linkedin.com/in/liza/'
+  }
+};
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      {/* Pass 'name', 'city', and 'color' as props to Home */}
+      <Home name={user.name} city={user.city} color={user.color} />
+      
+      {/* Pass 'bio', 'github', and 'linkedin' as props to About */}
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} />
     </div>
   );
 }
 
 export default App;
+
